fda1f7b8be95846064ceb75e49193388378a1b59
tonylarren <77399016+tonylarren@users.noreply.github.com>
2021-01-13 18:07:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html>
+	<head>
+		<title>Connect</title>
+		<meta charset="utf-8" />
+		<link href="style.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<h1>Site test GITHUB!!!!!</h1>
+	</body>
+</html>
